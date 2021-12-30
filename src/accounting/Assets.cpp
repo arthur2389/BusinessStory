b13791820c0bdbea1bdf528426abcf94b37164b6
Arthur <artur.ferber@wdc.com>
2021-12-30 11:33:40 +0200

    FEATURE: add to folder
    Signed-off-by: Arthur <artur.ferber@wdc.com>

--- src/accounting/Assets.cpp
+++ src/accounting/Assets.cpp
@@ -5,22 +5,22 @@ Assets::Assets(int equity, int debt)
 
 Assets::Assets(Assets& source) : Assets{source.get_equity(), source.get_debt()} {}
 
-int Assets::get_equity() {return m_equity}
+int Assets::get_equity() {return m_equity;}
 
-int Assets::get_debt() {return m_debt}
+int Assets::get_debt() {return m_debt;}
 
-int Assets::total_assests() {return m_debt + m_equity}
+int Assets::total_assests() {return m_debt + m_equity;}
 
-int Assets::equity_to_debt_ratio() {return m_equity / m_debt}
+int Assets::equity_to_debt_ratio() {return m_equity / m_debt;}
 
 int Assets::add_equity(int add_to_equity)
 {
     m_equity += add_to_equity;
-    return m_equity
+    return m_equity;
 }
 
 int Assets::add_debt(int add_to_debt)
 {
     m_debt += add_to_debt;
-    return m_debt
+    return m_debt;
 }
